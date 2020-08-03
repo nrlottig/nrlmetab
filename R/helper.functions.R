8371076b563450b12be7f35fb762187b35767cc9
Noah Lottig <nrlottig@wisc.edu>
2020-08-03 13:25:36 -0500

    added find pch function

--- R/helper.functions.R
+++ R/helper.functions.R
@@ -21,3 +21,18 @@ date2doy <- function(x){
     frac <- as.numeric(difftime(x, midnight, units="days"))
     day+frac
 }
+#function to remove points
+identifyPch <- function(x, y = NULL, n = 2, pch = 19, ...)
+{
+    xy <- xy.coords(x, y); x <- xy$x; y <- xy$y
+    sel <- rep(FALSE, length(x)); res <- integer(0)
+    while(sum(sel) < n) {
+        ans <- identify(x[!sel], y[!sel], n = 1, plot = FALSE, ...)
+        if(!length(ans)) break
+        ans <- which(!sel)[ans]
+        points(x[ans], y[ans], pch = pch)
+        sel[ans] <- TRUE
+        res <- c(res, ans)
+    }
+    res
+}
